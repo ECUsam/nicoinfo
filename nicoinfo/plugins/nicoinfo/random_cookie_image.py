c3b8d8b6d73ffdd2e0a685196c17660bba87063a
ECUsam <wangyanzhao2003@gmail.com>
2024-02-18 23:59:27 +0800

    test

--- nicoinfo/plugins/nicoinfo/random_cookie_image.py
+++ nicoinfo/plugins/nicoinfo/random_cookie_image.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import random
+import time
 
 # from aiohttp import ClientSession
 from nonebot.adapters.onebot.v11 import Bot, Event
@@ -17,10 +18,9 @@ except ValueError:
     from wraps import retry_async
 
 header = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36',
-    'Cookie': 'nicosid=1633700428.452220814; _ts_yjad=1633700428228; _ss_pp_id=9b353ec49aa360431471646182137066; _cc_id=f58d08fa37a8aaae434b30e83797ecfd; lang=ja-jp; area=JP; _ga_8W314HNSE8=GS1.1.1653933007.94.1.1653934013.0; _gcl_au=1.1.553504617.1659349543; pbjs_sharedId=59bf20ff-a27d-4f03-804f-b7920a216078; _fbp=fb.1.1665462893682.174282674; __gads=ID=2d7a6b88f86b06c7:T=1633700444:S=ALNI_MZV9KgdWjSDI3uzQgPo9m7Ep8C-UA; _td=ceef4192-d12e-43ba-b7c5-ed24cc177792; common-header-oshirase-open-date=2022-10-25T13:04:19.965Z; __gpi=UID=00000450e6f57b88:T=1649410308:RT=1666703067:S=ALNI_MYaaH1zOxhAP4tI_sasBqNVtWHvpQ; nico_gc=tg_s%3Df%26tg_o%3Dd; cto_bundle=0-T18V94MmR6am1ZWmVJOWt4YmY1NE5MYW5FTEtsOVRrJTJGMEklMkIwY3lPbnlSZnJsbjlWTWFDdWFweEdLeFlDT25BWXo1U29nbVNWTHRSamZKOXNRdjElMkJDcUVTZE1uVU1PVmxzSFhZT1RUTW9vcHo0UHUzdFR6YlBjQ29rNFA3Vm9BZDBCVmI5c3IwZXNIZUFLdWttVEF3MXVReXclM0QlM0Q; optimizelyEndUserId=oeu1666928747475r0.3711834985684135; _gid=GA1.2.1402785906.1667051185; _ga_7W6WKHGQTW=GS1.1.1667051183.4.1.1667051767.58.0.0; _ga_G7379W9VJ0=GS1.1.1667051183.4.1.1667051767.60.0.0; _gat_UA-88451119-5=1; _gat_NicoGoogleTagManager=1; mfa_session=87722391_m8gVOCWbbtAtvvswitvaVU2KIKiJueXI; _ga=GA1.1.747558350.1633700427; user_session=user_session_87722391_f3b01de072c501c84b48f8005b2a74be53bde8445d11f1469bfd8f6bf548eb62; user_session_secure=ODc3MjIzOTE6TTVKNll2ajhCZC5TVTVROU1UaHhKMnFRNXV6dWFWeW9vZXZ4SUUxNDVaVA; _ga_5LM4HED1NJ=GS1.1.1667051185.267.1.1667051825.1.0.0'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
+    'Cookie': 'nicosid=1633700428.452220814; _ts_yjad=1633700428228; _ss_pp_id=9b353ec49aa360431471646182137066; _cc_id=f58d08fa37a8aaae434b30e83797ecfd; lang=ja-jp; area=JP; _ga_8W314HNSE8=GS1.1.1653933007.94.1.1653934013.0; _gcl_au=1.1.553504617.1659349543; pbjs_sharedId=59bf20ff-a27d-4f03-804f-b7920a216078; _fbp=fb.1.1665462893682.174282674; __gads=ID=2d7a6b88f86b06c7:T=1633700444:S=ALNI_MZV9KgdWjSDI3uzQgPo9m7Ep8C-UA; _td=ceef4192-d12e-43ba-b7c5-ed24cc177792; common-header-oshirase-open-date=2022-10-25T13:04:19.965Z; __gpi=UID=00000450e6f57b88:T=1649410308:RT=1666703067:S=ALNI_MYaaH1zOxhAP4tI_sasBqNVtWHvpQ; nico_gc=tg_s%3Df%26tg_o%3Dd; cto_bundle=0-T18V94MmR6am1ZWmVJOWt4YmY1NE5MYW5FTEtsOVRrJTJGMEklMkIwY3lPbnlSZnJsbjlWTWFDdWFweEdLeFlDT25BWXo1U29nbVNWTHRSamZKOXNRdjElMkJDcUVTZE1uVU1PVmxzSFhZT1RUTW9vcHo0UHUzdFR6YlBjQ29rNFA3Vm9BZDBCVmI5c3IwZXNIZUFLdWttVEF3MXVReXclM0QlM0Q; optimizelyEndUserId=oeu1666928747475r0.3711834985684135; _gid=GA1.2.1402785906.1667051185; _ga_7W6WKHGQTW=GS1.1.1667051183.4.1.1667051767.58.0.0; _ga_G7379W9VJ0=GS1.1.1667051183.4.1.1667051767.60.0.0; _gat_UA-88451119-5=1; _gat_NicoGoogleTagManager=1; mfa_session=87722391_m8gVOCWbbtAtvvswitvaVU2KIKiJueXI; _ga=GA1.1.747558350.1633700427; user_session=user_session_87722391_6dc20b4a32e0a8614c9084dd66a698c4fb552c1b9a8fad0a79e59a2437384532; user_session_secure=ODc3MjIzOTE6a3JiSWRWNkw5bUsybml5dThwQzRsd3FIQlVqdXRnSDE4ZlV1OFZyUEI3ZQ; _ga_5LM4HED1NJ=GS1.1.1667051185.267.1.1667051825.1.0.0'
 }
-
 
 @retry_async(retries=3, delay=1)
 async def get_im_date_info(id):
@@ -175,7 +175,7 @@ class Cookie_image_getter:
     async def initial(self):
         self.check_usable()
         print("初始化")
-        self.load_sended()
+        # self.load_sended()
         # try:
         #     await self.get_cookie_elements_to_num(max_page=self.max_page)
         #     print(self.random_cookie_list)
@@ -285,6 +285,35 @@ class Cookie_image_getter:
 
 import threading
 
+def get_url_im_download_new(im):
+    id = im.split("im")[1]
+    url = f"https://seiga.nicovideo.jp/image/source/{id}"
+    text = requests.get(url, headers=header).text
+    soup = BeautifulSoup(text, "html.parser")
+    div = soup.find("div", {"class": "illust_view_big"})
+    data_src = div.get("data-src")
+    return data_src
+print(get_url_im_download_new("im11375651"))
+def download_image_new(url, local_path):
+    response = requests.get(url, headers=header)
+    if response.status_code == 200:
+        directory = os.path.dirname(local_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        with open(local_path, 'wb') as file:
+            file.write(response.content)
+    else:
+        return False
+def download_with_im_new(im, local_path="image"):
+    print("下载", im)
+    data_src = get_url_im_download_new(im)
+    # 使用 os.path.join 来创建路径
+    path = os.path.abspath(os.path.join(local_path, im))
+    download_image_new(data_src, path)
+
+def download_muti_im_new(im_list: list, local="image"):
+    for im in im_list:
+        download_with_im_new(im, local)
 
 class cookie_download_thread(threading.Thread):
     def __init__(self, tag="クッキー☆", max_page=100):
@@ -297,19 +326,17 @@ class cookie_download_thread(threading.Thread):
         while len(self.random_cookie_list) < 20:
             self.random_cookie_list += get_cookie_elements(self.tag, max_page=self.max_page)
 
+
     def run(self):
         if not os.path.exists("image_cookie"):
             os.mkdir("image_cookie")
         while True:
             self.check_reload()
             image_num = len(os.listdir("image_cookie"))
-            if image_num < 8:
+            if image_num < 20:
                 selected_elements = random.sample(self.random_cookie_list, 5)
                 self.random_cookie_list = [elem for elem in self.random_cookie_list if elem not in selected_elements]
-                download_muti_im(selected_elements, local="image_cookie")
+                download_muti_im_new(selected_elements, local="image_cookie")
+            time.sleep(1)
 
 
-if __name__ == '__main__':
-    b = Cookie_image_getter()
-    asyncio.run(b.initial())
-    pass
--- test.py
+++ test.py
@@ -1,33 +1,18 @@
-import threading
-import time
 import requests
+from bs4 import BeautifulSoup
+header = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
+    'Cookie': 'nicosid=1633700428.452220814; _ts_yjad=1633700428228; _ss_pp_id=9b353ec49aa360431471646182137066; _cc_id=f58d08fa37a8aaae434b30e83797ecfd; lang=ja-jp; area=JP; _ga_8W314HNSE8=GS1.1.1653933007.94.1.1653934013.0; _gcl_au=1.1.553504617.1659349543; pbjs_sharedId=59bf20ff-a27d-4f03-804f-b7920a216078; _fbp=fb.1.1665462893682.174282674; __gads=ID=2d7a6b88f86b06c7:T=1633700444:S=ALNI_MZV9KgdWjSDI3uzQgPo9m7Ep8C-UA; _td=ceef4192-d12e-43ba-b7c5-ed24cc177792; common-header-oshirase-open-date=2022-10-25T13:04:19.965Z; __gpi=UID=00000450e6f57b88:T=1649410308:RT=1666703067:S=ALNI_MYaaH1zOxhAP4tI_sasBqNVtWHvpQ; nico_gc=tg_s%3Df%26tg_o%3Dd; cto_bundle=0-T18V94MmR6am1ZWmVJOWt4YmY1NE5MYW5FTEtsOVRrJTJGMEklMkIwY3lPbnlSZnJsbjlWTWFDdWFweEdLeFlDT25BWXo1U29nbVNWTHRSamZKOXNRdjElMkJDcUVTZE1uVU1PVmxzSFhZT1RUTW9vcHo0UHUzdFR6YlBjQ29rNFA3Vm9BZDBCVmI5c3IwZXNIZUFLdWttVEF3MXVReXclM0QlM0Q; optimizelyEndUserId=oeu1666928747475r0.3711834985684135; _gid=GA1.2.1402785906.1667051185; _ga_7W6WKHGQTW=GS1.1.1667051183.4.1.1667051767.58.0.0; _ga_G7379W9VJ0=GS1.1.1667051183.4.1.1667051767.60.0.0; _gat_UA-88451119-5=1; _gat_NicoGoogleTagManager=1; mfa_session=87722391_m8gVOCWbbtAtvvswitvaVU2KIKiJueXI; _ga=GA1.1.747558350.1633700427; user_session=user_session_87722391_6dc20b4a32e0a8614c9084dd66a698c4fb552c1b9a8fad0a79e59a2437384532; user_session_secure=ODc3MjIzOTE6a3JiSWRWNkw5bUsybml5dThwQzRsd3FIQlVqdXRnSDE4ZlV1OFZyUEI3ZQ; _ga_5LM4HED1NJ=GS1.1.1667051185.267.1.1667051825.1.0.0'
+}
 
-# 定义一个下载函数，模拟下载操作
-def download(url):
-    print(f"Downloading {url}")
-    # 模拟下载操作，这里使用 requests 库发送一个简单的 GET 请求
-    response = requests.get(url)
-    print(f"Downloaded {url}")
+def get_url_im_download(im):
+    id = im.split("im")[1]
+    url = f"https://seiga.nicovideo.jp/image/source/{id}"
+    print(url)
+    text = requests.get(url, headers=header).text
+    soup = BeautifulSoup(text, "html.parser")
+    div = soup.find("div", {"class": "illust_view_big"})
+    data_src = div.get("data-src")
+    return data_src
 
-# 创建线程类
-class DownloadThread(threading.Thread):
-    def __init__(self, url):
-        threading.Thread.__init__(self)
-        self.url = url
-
-    def run(self):
-        print(time.time())
-        download(self.url)
-
-# 创建并启动两个线程，每个线程下载不同的内容
-thread1 = DownloadThread("https://www.example.com/file1")
-thread2 = DownloadThread("https://www.example.com/file2")
-
-thread1.start()
-thread2.start()
-print(111)
-# 主线程等待两个线程执行完成
-thread1.join()
-thread2.join()
-
-print("All downloads are completed!")
+print(get_url_im_download("im11375651"))
